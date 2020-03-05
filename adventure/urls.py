279c799efc8ba3c1a1b0ec36da35d4363af3dc51
Matthew Nelson <05nelsonm@protonmail.com>
2020-03-05 14:14:40 -0500

    add url to endpoints

--- adventure/urls.py
+++ adventure/urls.py
@@ -5,5 +5,6 @@ urlpatterns = [
     url('init', api.initialize),
     url('move', api.move),
     url('say', api.say),
+    url(r'^room/(?P<room_id>\d+)$', api.getRoomById),
     url('rooms', api.rooms)
 ]
